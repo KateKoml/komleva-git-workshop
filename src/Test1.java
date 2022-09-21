146aa15b470d261cf8425e1210f5cf18fa418f8f
Kate Komleva <komlevakate99@gmail.com>
2022-09-21 13:28:54 +0300

    feat: add solution to merge conflict

--- src/Test1.java
+++ src/Test1.java
@@ -1,6 +1,11 @@
+import java.util.*;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("This is a workshop!");
-        System.out.println("We need to calculate price");
+        System.out.println("Enter x and y");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(x + y);
     }
 }
