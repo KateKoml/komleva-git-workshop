b48ffbf4d503f854b86a390aa3c24a13f242af73
Kate Komleva <komlevakate99@gmail.com>
2022-09-21 12:13:02 +0300

    commit before merge

--- src/Test1.java
+++ src/Test1.java
@@ -1,6 +1,6 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("This is a workshop!");
-        System.out.println("We need to calculate prise");
+        System.out.println("We need to calculate price");
     }
 }
